4d4b5fdd68e56a0881470ee0fbdc7cd8701a888b
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-09-20 19:59:26 +0100

    update

--- MyApp/src/main/java/org/example/Main.java
+++ MyApp/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.helpers.DatabaseConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DatabaseConnection.getInstance().getConnection();
     }
 }
--- MyApp/src/main/java/org/example/helpers/DatabaseConnection.java
+++ MyApp/src/main/java/org/example/helpers/DatabaseConnection.java
@@ -1,4 +1,35 @@
 package org.example.helpers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private Connection connection;
+    private static DatabaseConnection instance;
+    private final String url = "jdbc:postgresql://localhost/dvdrental";
+    private final String user = "postgres";
+    private final String password = "root";
+    private DatabaseConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Connected");
+            this.connection = DriverManager.getConnection(url,user,password);
+        } catch (Exception ex) {
+            System.out.println("Not Connected");
+            System.out.println(ex);
+            this.connection = null;
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance() {
+        if(instance == null){
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
 }
